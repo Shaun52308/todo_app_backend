b95c3c64bcbcf7b2d64e1a601f0d6d3e70d8f83f
Shaun Abraham <shaun.abrahom@gmail.com>
2017-10-01 16:10:05 +0000

    add rcav flow for the new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'todo/index' , to:'todo#index'
   get 'todo/show' , to:'todo#show'
+  get 'todo/new', to: 'todo#new'
+  post 'todo/new' , to: 'todo#create'
+  get 'todo/create', to: 'todo#create'
   get 'todo/show/:id' , to:'todo#show'
+  
 end 
 
